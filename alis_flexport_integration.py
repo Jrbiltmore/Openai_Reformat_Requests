fb582b38396189005779a5686bf1961f1a2535d9
Jacob Thomas Redmond <127049997+Jrbiltmore@users.noreply.github.com>
2023-07-15 10:03:27 -0700

    Update alis_flexport_integration.py
    This script provides functionality for integrating ALIS (Algorithms for Logistics Intelligence and Security) and Flexport systems. It includes functions for error handling, request reformatting, fallback logic, data validation, and sending data to the ALIS and Flexport APIs. The integration allows for seamless communication between the two systems, enabling efficient data exchange and collaboration.
    
    Author: Jacob Thomas Messer
    Contact: jrbiltmore@icloud.com

--- alis_flexport_integration.py
+++ alis_flexport_integration.py
@@ -1,50 +1,134 @@
 """
 ALIS-Flexport Integration
+
+This script provides functionality for integrating ALIS (Algorithms for Logistics Intelligence and Security) and Flexport systems. It includes functions for error handling, request reformatting, fallback logic, data validation, and sending data to the ALIS and Flexport APIs. The integration allows for seamless communication between the two systems, enabling efficient data exchange and collaboration.
+
 Author: Jacob Thomas Messer
 Contact: jrbiltmore@icloud.com
 """
 
-import requests
 import openai
 import logging
+import requests
 
+# Set up OpenAI API key
 openai.api_key = 'YOUR_API_KEY'
 
-def handle_data_requirements(data):
+# ALIS API URL
+ALIS_API_URL = 'https://alis.example.com/api'
+
+# Flexport API URL
+FLEXPORT_API_URL = 'https://flexport.example.com/api'
+
+# Flexport Financial API URL
+FLEXPORT_FINANCIAL_API_URL = 'https://flexport-financial.example.com/api'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def handle_error(error_message):
     """
-    Handles the given data by sending it to the OpenAI API
-    and retrieving suggestions on the data requirements.
+    Handle error messages using the OpenAI API.
 
     Args:
-        data (dict): The data to analyze.
+        error_message (str): The error message to handle.
 
     Returns:
-        list: A list of suggested data requirements.
+        list: A list of suggestions for error handling.
     """
     try:
         response = openai.Completion.create(
             engine='text-davinci-003',
-            prompt=f"Data: {data}\nData requirements:",
+            prompt=f"Error: {error_message}\nHow to handle this error:",
             max_tokens=50,
             n=3,
             stop=None,
             temperature=0.5
         )
-
-        data_requirements = [choice['text'].strip() for choice in response.choices] if 'choices' in response else []
-
-        return data_requirements
-
+        
+        suggestions = [choice['text'].strip() for choice in response.choices] if 'choices' in response else []
+        
+        return suggestions
+        
     except openai.error.OpenAIError as e:
         logging.error(f"OpenAI API error occurred: {str(e)}")
+        return []
+    except openai.error.APIConnectionError as e:
+        logging.error(f"API connection error occurred: {str(e)}")
+        return []
+    except openai.error.AuthenticationError as e:
+        logging.error(f"Authentication error occurred: {str(e)}")
+        return []
+    except openai.error.RateLimitError as e:
+        logging.error(f"Rate limit error occurred: {str(e)}")
         return []
     except Exception as e:
         logging.error(f"Error occurred: {str(e)}")
         return []
 
+def reformat_request(request_text):
+    """
+    Reformat a request using the OpenAI API.
+
+    Args:
+        request_text (str): The request text to reformat.
+
+    Returns:
+        list: A list of reformatted suggestions.
+    """
+    try:
+        response = openai.Completion.create(
+            engine='text-davinci-003',
+            prompt=f"Request: {request_text}\nHow to reformat this request:",
+            max_tokens=50,
+            n=3,
+            stop=None,
+            temperature=0.5
+        )
+        
+        reformatted_suggestions = [choice['text'].strip() for choice in response.choices] if 'choices' in response else []
+        
+        return reformatted_suggestions
+        
+    except openai.error.OpenAIError as e:
+        logging.error(f"OpenAI API error occurred: {str(e)}")
+        return []
+    except openai.error.APIConnectionError as e:
+        logging.error(f"API connection error occurred: {str(e)}")
+        return []
+    except openai.error.AuthenticationError as e:
+        logging.error(f"Authentication error occurred: {str(e)}")
+        return []
+    except openai.error.RateLimitError as e:
+        logging.error(f"Rate limit error occurred: {str(e)}")
+        return []
+    except Exception as e:
+        logging.error(f"Error occurred: {str(e)}")
+        return []
+
+def fallback_handle_error(error_message):
+    """
+    Implement custom fallback logic to handle error messages.
+
+    Args:
+        error_message (str): The error message to handle.
+    """
+    logging.warning("API call failed. Implement fallback logic here.")
+    # ...
+
+def fallback_reformat_request(request_text):
+    """
+    Implement custom fallback logic to handle request reformatting.
+
+    Args:
+        request_text (str): The request text to handle.
+    """
+    logging.warning("API call failed. Implement fallback logic here.")
+    # ...
+
 def send_data_to_alis(data):
     """
-    Sends the given data to the ALIS API for processing.
+    Send data to the ALIS API.
 
     Args:
         data (dict): The data to send.
@@ -62,7 +146,7 @@ def send_data_to_alis(data):
 
 def send_data_to_flexport(data):
     """
-    Sends the given data to the Flexport API for processing.
+    Send data to the Flexport API.
 
     Args:
         data (dict): The data to send.
@@ -80,7 +164,7 @@ def send_data_to_flexport(data):
 
 def send_data_to_flexport_financial(data):
     """
-    Sends the given data to the Flexport Financial API for processing.
+    Send data to the Flexport Financial API.
 
     Args:
         data (dict): The data to send.
@@ -96,38 +180,66 @@ def send_data_to_flexport_financial(data):
         logging.error(f"Flexport Financial integration error: {str(e)}")
         return None
 
-# Set up logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+def validate_flexport_financial_data(data):
+    """
+    Validates the Flexport Financial data before sending it for integration.
 
-# Usage example
-data = {
+    Args:
+        data (dict): The data to validate.
+
+    Returns:
+        bool: True if the data is valid, False otherwise.
+    """
+    # Implement custom validation logic
+    # ...
+
+# Usage examples
+
+# Example 1: Handling errors
+error_message = "Error: Invalid input"
+suggestions = handle_error(error_message)
+if suggestions:
+    logging.info("Error handling suggestions:", suggestions)
+else:
+    fallback_handle_error(error_message)
+
+# Example 2: Reformatting requests
+request_text = "Request: Invalid input"
+reformatted_suggestions = reformat_request(request_text)
+if reformatted_suggestions:
+    logging.info("Request reformatting suggestions:", reformatted_suggestions)
+else:
+    fallback_reformat_request(request_text)
+
+# Example 3: Sending data to ALIS
+alis_data = {
     "key": "value"
 }
-
-# Handle data requirements using OpenAI
-data_requirements = handle_data_requirements(data)
-if data_requirements:
-    logging.info("Data requirements:", data_requirements)
-else:
-    logging.warning("Data requirements not available")
-
-# Send data to ALIS
-alis_response = send_data_to_alis(data)
+alis_response = send_data_to_alis(alis_data)
 if alis_response:
     logging.info("ALIS integration success")
 else:
-    logging.warning("ALIS integration failed")
+    logging.error("ALIS integration failed")
 
-# Send data to Flexport
-flexport_response = send_data_to_flexport(data)
+# Example 4: Sending data to Flexport
+flexport_data = {
+    "key": "value"
+}
+flexport_response = send_data_to_flexport(flexport_data)
 if flexport_response:
     logging.info("Flexport integration success")
 else:
-    logging.warning("Flexport integration failed")
+    logging.error("Flexport integration failed")
 
-# Send data to Flexport Financial
-flexport_financial_response = send_data_to_flexport_financial(data)
-if flexport_financial_response:
-    logging.info("Flexport Financial integration success")
+# Example 5: Sending data to Flexport Financial
+financial_data = {
+    "key": "value"
+}
+if validate_flexport_financial_data(financial_data):
+    flexport_financial_response = send_data_to_flexport_financial(financial_data)
+    if flexport_financial_response:
+        logging.info("Flexport Financial integration success")
+    else:
+        logging.error("Flexport Financial integration failed")
 else:
-    logging.warning("Flexport Financial integration failed")
+    logging.error("Invalid Flexport Financial data")
